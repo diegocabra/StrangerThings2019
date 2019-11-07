71692c7ee740ab3e6ee3c75c0e06e921ea447a9a
EmaEGR <emapda011@gmail.com>
2019-11-07 17:41:31 -0300

    Ema

--- src/Objetos/Bomba.java
+++ src/Objetos/Bomba.java
@@ -5,6 +5,7 @@ import javax.swing.ImageIcon;
 
 import Principal.Celda;
 
+
  
 
 public class Bomba extends Objeto {
@@ -28,16 +29,26 @@ public class Bomba extends Objeto {
 		*/
 	}
 	
+	public Bomba(Celda c) {
+		super(c);
+		vida=Integer.MAX_VALUE;
+		grafico.setIcon(new ImageIcon(this.getClass().getResource("/Sprites/bomba.gif")));
+		
+		estallar = new Estallar(this,miCelda);
+		estallar.start();
+		
+	}
+	
 	
 	public void setCelda(Celda c) {
 		miCelda=c;
 		estallar.start();
 	}
-	/*
+	
 	
 	public void estallar()
 	{
 		grafico.setIcon(new ImageIcon(this.getClass().getResource("/Sprites/Explosion.gif")));
 	}
-	*/
+	
 }
