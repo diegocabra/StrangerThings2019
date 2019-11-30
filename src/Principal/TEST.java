9e6ca108b833028fb7e737cf9a0e3b18e50e133f
msenderovsky <msenderovsky@gmail.com>
2019-11-29 23:28:58 -0300

    eliminado 1 test

--- src/Principal/TEST.java
+++ src/Principal/TEST.java
@@ -1,7 +1,5 @@
 package Principal;
 
- 
-
 public class TEST {
 	public static void main(String[] args) {		
 		Juego l= new Juego(6,10);
--- src/Principal/Test2.java
+++
@@ -1,8 +0,0 @@
-package Principal;
-
-public class Test2 {
-	public static void main(String[] args) {		
-	Juego l= new Juego(6,10);
-	}
-}
-
